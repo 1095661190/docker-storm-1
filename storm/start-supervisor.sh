fd5a171e7f10b131ed020367a72e4cbe16e77c4d
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-21 17:32:07 +0100

    initial commit

---
+++ storm/start-supervisor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download storm
+wget -q -N http://mirrors.gigenet.com/apache/storm/${STORM_VERSION}/${STORM_VERSION}.zip
+unzip -o ${STORM_VERSION}.zip -d /usr/share/
+rm ${STORM_VERSION}.zip
+
+# Now copy config files
+cp -a -rv /mnt/storm/. .
+
+# append own hostname to config file
+echo "storm.local.hostname: `hostname -i`" | tee -a conf/storm.yaml
+
+exec bin/storm "$@"
