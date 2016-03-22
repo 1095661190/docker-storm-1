91b844c2e8309f4ff85fba6db5ec982bc0161197
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-22 11:53:10 +0100

    testing out

--- storm/entrypoint.sh
+++ storm/entrypoint.sh
@@ -5,7 +5,8 @@ wget -q -N http://mirrors.gigenet.com/apache/storm/${STORM_VERSION}/${STORM_VERS
 sudo unzip -o ${STORM_VERSION}.zip -d /usr/share/
 rm ${STORM_VERSION}.zip
 
-sudo mv /usr/share/${STORM_VERSION} /usr/share/storm
+# sudo mv /usr/share/${STORM_VERSION} /usr/share/storm
+sudo ln -s /usr/share/${STORM_VERSION} /usr/share/storm
 
 # Now copy config files
 cp -a -rv /mnt/storm/. .
@@ -14,5 +15,3 @@ cp -a -rv /mnt/storm/. .
 echo "storm.local.hostname: `hostname -i`" | tee -a conf/storm.yaml
 
 exec bin/storm "$@"
-
-STORM_VERSION=apache-storm-0.10.0
