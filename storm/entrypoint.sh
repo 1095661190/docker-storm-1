f246d10d1447e1234f3ac1172d8d0207e817b255
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-22 11:57:27 +0100

    testing out

--- storm/entrypoint.sh
+++ storm/entrypoint.sh
@@ -2,11 +2,12 @@
 
 # download storm
 wget -q -N http://mirrors.gigenet.com/apache/storm/${STORM_VERSION}/${STORM_VERSION}.zip
-sudo unzip -o ${STORM_VERSION}.zip -d /usr/share/
+unzip -o ${STORM_VERSION}.zip -d .
 rm ${STORM_VERSION}.zip
 
 # sudo mv /usr/share/${STORM_VERSION} /usr/share/storm
-sudo ln -s /usr/share/${STORM_VERSION} /usr/share/storm
+# sudo ln -s /usr/share/storm /usr/share/${STORM_VERSION}
+cd ${STORM_VERSION}
 
 # Now copy config files
 cp -a -rv /mnt/storm/. .
