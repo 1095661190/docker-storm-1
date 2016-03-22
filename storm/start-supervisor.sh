f0d68fd816209233517dbcf1f1ba8420949c8c50
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-22 09:22:54 +0100

    fix

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -13,4 +13,4 @@ cp -a -rv /mnt/storm/. .
 # append own hostname to config file
 echo "storm.local.hostname: `hostname -i`" | tee -a conf/storm.yaml
 
-exec bin/storm "$@"
+exec /usr/share/storm/bin/storm "$@"
