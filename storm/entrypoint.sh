b4be49dffd20c5baf52a37a217962399636307ac
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-22 23:11:18 +0100

    ZooKeeper servers may now be specified via ZOOKEEPER_SERVERS environment variable when calling docker run

--- storm/entrypoint.sh
+++ storm/entrypoint.sh
@@ -3,7 +3,26 @@
 # Now copy config files
 cp -a -rv /mnt/storm/. .
 
-# append own hostname to config file
-echo "storm.local.hostname: `hostname -i`" | tee -a conf/storm.yaml
+############################
+# storm.zookeeper.servers #
+############################
+if [ -z "$ZOOKEEPER_SERVERS" ]
+then
+    echo -e "Please provide \e[1;35mZOOKEEPER_SERVERS\e[0m like \"1.2.3.4\" or \"1.2.3.4,5.6.7.8\""
+    return
+fi
 
-exec bin/storm "$@"
+# All ZooKeeper server IPs in an array
+IFS=', ' read -r -a ZOOKEEPER_SERVERS_ARRAY <<< "$ZOOKEEPER_SERVERS"
+ZOOKEEPER_SERVERS_ESCAPED=
+for index in "${!ZOOKEEPER_SERVERS_ARRAY[@]}"
+do
+    ZOOKEEPER_SERVERS_ESCAPED="$ZOOKEEPER_SERVERS_ESCAPED,${ZOOKEEPER_SERVERS_ARRAY[index]}"
+done
+
+ZOOKEEPER_SERVERS_ESCAPED="["${ZOOKEEPER_SERVERS_ESCAPED:1}"]"
+
+
+exec bin/storm "$@" -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
+   -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
+   -c storm.local.hostname=$(hostname -i | awk '{print $1;}')
