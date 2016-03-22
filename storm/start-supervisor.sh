fd0142ecdbb6a9cbeb94b8879f307052fd9aebfe
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-22 09:25:56 +0100

    fix

--- storm/start-supervisor.sh
+++ storm/start-supervisor.sh
@@ -5,7 +5,7 @@ wget -q -N http://mirrors.gigenet.com/apache/storm/${STORM_VERSION}/${STORM_VERS
 unzip -o ${STORM_VERSION}.zip -d /usr/share/
 rm ${STORM_VERSION}.zip
 
-sudo ln -s /usr/share/${STORM_VERSION} /usr/share/storm
+mv /usr/share/${STORM_VERSION} /usr/share/storm
 
 # Now copy config files
 cp -a -rv /mnt/storm/. .
@@ -13,4 +13,4 @@ cp -a -rv /mnt/storm/. .
 # append own hostname to config file
 echo "storm.local.hostname: `hostname -i`" | tee -a conf/storm.yaml
 
-exec /usr/share/storm/bin/storm "$@"
+exec bin/storm "$@"
