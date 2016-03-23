66b95079f35e78d45c15843060c6a9fb95bdb0d5
Wolfram Wingerath <wingerath@informatik.uni-hamburg.de>
2016-03-23 10:47:20 +0100

    Refactoring

--- storm/entrypoint.sh
+++ storm/entrypoint.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-# Now copy config files
-cp -a -rv /mnt/storm/. .
+# Now copy config files, if there are any
+if [ -d "/mnt/storm/" ]; then
+    cp -a -rv /mnt/storm/. .
+fi
 
 ############################
 # storm.zookeeper.servers #
 ############################
-if [ -z "$ZOOKEEPER_SERVERS" ]
-then
-    echo -e "Please provide \e[1;35mZOOKEEPER_SERVERS\e[0m like \"1.2.3.4\" or \"1.2.3.4,5.6.7.8\""
-    return
+if ! [ -z "$ZOOKEEPER_SERVERS" ]; then
+    # All ZooKeeper server IPs in an array
+    IFS=', ' read -r -a ZOOKEEPER_SERVERS_ARRAY <<< "$ZOOKEEPER_SERVERS"
+    ZOOKEEPER_SERVERS_ESCAPED=
+    for index in "${!ZOOKEEPER_SERVERS_ARRAY[@]}"
+    do
+        ZOOKEEPER_SERVERS_ESCAPED="$ZOOKEEPER_SERVERS_ESCAPED,${ZOOKEEPER_SERVERS_ARRAY[index]}"
+    done
+    ZOOKEEPER_SERVERS_ESCAPED="["${ZOOKEEPER_SERVERS_ESCAPED:1}"]"
 fi
 
-# All ZooKeeper server IPs in an array
-IFS=', ' read -r -a ZOOKEEPER_SERVERS_ARRAY <<< "$ZOOKEEPER_SERVERS"
-ZOOKEEPER_SERVERS_ESCAPED=
-for index in "${!ZOOKEEPER_SERVERS_ARRAY[@]}"
-do
-    ZOOKEEPER_SERVERS_ESCAPED="$ZOOKEEPER_SERVERS_ESCAPED,${ZOOKEEPER_SERVERS_ARRAY[index]}"
-done
 
-ZOOKEEPER_SERVERS_ESCAPED="["${ZOOKEEPER_SERVERS_ESCAPED:1}"]"
-
-
-exec bin/storm "$@" -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
-   -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
-   -c storm.local.hostname=$(hostname -i | awk '{print $1;}')
+exec bin/storm "$@" \
+    -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
+    -c storm.zookeeper.servers=$ZOOKEEPER_SERVERS_ESCAPED \
+    -c storm.local.hostname=$(hostname)
